a68db20579db6d8062a871dae0bdc5a3444afcd7
mallum <mallum@b067294f-1dea-0310-9683-c47a78595994>
2005-06-08 21:53:17 +0000

    back out previous attempted transient infinite loop fix
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-window-manager@1173 b067294f-1dea-0310-9683-c47a78595994

--- src/dialog_client.c
+++ src/dialog_client.c
@@ -240,8 +240,7 @@ dialog_client_show(Client *c)
       int urgent_flag = 0; /* (c->flags & CLIENT_HAS_URGENCY_FLAG) ?
 			      CLIENT_HAS_URGENCY_FLAG : 0; */
 
-      while (lowest_trans->trans != NULL 
-	     && lowest_trans != lowest_trans->trans) 
+      while (lowest_trans->trans != NULL) 
 	lowest_trans = lowest_trans->trans;
 
       highest_client = client_get_highest_transient(lowest_trans, urgent_flag);
